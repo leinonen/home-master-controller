2061d7137bfc7c757fe599fd154dc4ae04d4108f
Peter Leinonen <peter@leinonen.se>
2015-04-28 20:35:46 +0200

    show devices and groups

--- app.js
+++ app.js
@@ -1,9 +1,28 @@
 var telldus = require('./telldus');
 
-telldus.doCall('devices/list', { supportedMethods: 1 })
-.then(function(response){
-  console.log(response);
-})
-.fail(function(error){
-  console.log(error);
-});
+telldus.doCall('devices/list', {supportedMethods: 1})
+	.then(function (response) {
+		var devices = response.json.device;
+
+		var groups = devices.filter(function (d) {
+			return d.type === 'group';
+		});
+
+		var hardware = devices.filter(function (d) {
+			return d.type === 'device';
+		});
+
+		console.log('--- Groups ---');
+		groups.forEach(function(group){
+			console.log(group.name);
+		});
+
+		console.log('--- Hardware ---');
+		hardware.forEach(function(device){
+			console.log(device.name);
+		});
+
+	})
+	.fail(function (error) {
+		console.log(error);
+	});
