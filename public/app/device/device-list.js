c9062a5cea667c0381a63061f4fb4381a0f909a8
Peter Leinonen <peter@leinonen.se>
2015-05-14 18:56:15 +0200

    refactored device list

--- public/app/device/device-list.js
+++ public/app/device/device-list.js
@@ -2,7 +2,7 @@
 
   var module = angular.module('device');
 
-  module.controller('DeviceListCtrl', function ($timeout, MasterApi) {
+  module.controller('DeviceListCtrl', function ($rootScope, $timeout, MasterApi) {
     var ctrl = this;
     ctrl.devices = [];
     ctrl.message = '';
@@ -18,51 +18,10 @@
 
     fetchDevices();
 
-    function control(id, params) {
-      MasterApi.control(id, params).then(function (status) {
-        console.log(status);
-        $timeout(fetchDevices, 100);
-      }).catch(console.error);
-    }
+    $rootScope.$on('fetchDevices', fetchDevices);
 
     ctrl.showDevices = function () {
       return ctrl.devices.length > 0;
-    };
-
-    ctrl.getState = function (device) {
-      if (device.motorized) {
-        return device.state.on ? 'Up' : 'Down';
-      } else {
-        return device.state.on ? 'On' : 'Off';
-      }
-    };
-
-    ctrl.buttonText = function (device) {
-      if (device.motorized) {
-        return device.state.on ? 'Bring Down' : 'Bring Up';
-      } else {
-        return device.state.on ? 'Turn Off' : 'Turn On';
-      }
-    };
-
-    ctrl.toggleDevice = function (device) {
-      if (device.state.on) {
-        control(device.id, {type: device.type, action: device.motorized ? 'down' : 'off'});
-      } else {
-        control(device.id, {type: device.type, action: device.motorized ? 'up' : 'on'});
-      }
-    };
-
-    ctrl.setBrightness = function (device) {
-      control(device.id, {type: device.type, action: 'bri', value: device.state.bri});
-    };
-
-    ctrl.setSaturation = function (device) {
-      control(device.id, {type: device.type, action: 'sat', value: device.state.sat});
-    };
-
-    ctrl.setHue = function (device) {
-      control(device.id, {type: device.type, action: 'hue', value: device.state.hue});
     };
 
   });
---
+++ public/app/device/telldus-device.js
@@ -0,0 +1,58 @@
+(function () {
+
+  var module = angular.module('device');
+
+  module.controller('TelldusDeviceCtrl', function ($scope, $rootScope, $timeout, MasterApi) {
+    var ctrl = this;
+    ctrl.device = $scope.device;
+
+    function fetchDevices() {
+      $rootScope.$emit('fetchDevices');
+    }
+
+    function control(id, params) {
+      MasterApi.control(id, params).then(function (status) {
+        $timeout(fetchDevices, 100);
+      }).catch(console.error);
+    }
+
+    ctrl.getState = function () {
+      if (ctrl.device.motorized) {
+        return ctrl.device.state.on ? 'Up' : 'Down';
+      } else {
+        return ctrl.device.state.on ? 'On' : 'Off';
+      }
+    };
+
+    ctrl.buttonText = function () {
+      if (ctrl.device.motorized) {
+        return ctrl.device.state.on ? 'Bring Down' : 'Bring Up';
+      } else {
+        return ctrl.device.state.on ? 'Turn Off' : 'Turn On';
+      }
+    };
+
+    ctrl.toggleDevice = function () {
+      if (ctrl.device.state.on) {
+        control(ctrl.device.id, {type: ctrl.device.type, action: ctrl.device.motorized ? 'down' : 'off'});
+      } else {
+        control(ctrl.device.id, {type: ctrl.device.type, action: ctrl.device.motorized ? 'up' : 'on'});
+      }
+    };
+
+
+  });
+
+  module.directive('telldusDevice', function () {
+    return {
+      scope: {
+        device: '='
+      },
+      replace: true,
+      templateUrl: 'app/device/telldus-device.html',
+      controller: 'TelldusDeviceCtrl',
+      controllerAs: 'ctrl'
+    };
+  });
+
+})();
