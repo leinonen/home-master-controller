b5c8bc9de7008f1e1a0972b89e4e61e457408263
Peter Leinonen <peter@leinonen.se>
2015-05-15 18:01:14 +0200

    create group gui stuff

--- public/app/group/group-create.js
+++ public/app/group/group-create.js
@@ -15,37 +15,49 @@
     ctrl.selectedGroups = [];
     ctrl.message = '';
 
+    ctrl.group = {
+      name: '',
+      items: []
+    };
+
+    function fetchDevicesAndGroups() {
+      MasterApi.getDevices().then(function (devices) {
+        ctrl.devices = devices;
+        ctrl.selectedDevice = ctrl.devices[0];
+      });
+
+      MasterApi.getGroups().then(function (groups) {
+        ctrl.groups = groups;
+        ctrl.selectedGroup = ctrl.groups[0];
+      });
+    }
+
+    fetchDevicesAndGroups();
+
+
     ctrl.addDevice = function () {
       var device = JSON.parse(ctrl.selectedDevice);
       ctrl.selectedDevices.push(device);
       ctrl.selectedDevice = {};
+      //ctrl.devices.splice(index, 1);
     };
 
     ctrl.addGroup = function () {
       var group = JSON.parse(ctrl.selectedGroup);
       ctrl.selectedGroups.push(group);
       ctrl.selectedGroup = {};
+      //ctrl.groups.splice(index, 1);
     };
 
-    MasterApi.getDevices().then(function (devices) {
-      ctrl.devices = devices;
-      ctrl.selectedDevice = ctrl.devices[0];
-    });
-
-    MasterApi.getGroups().then(function (groups) {
-      ctrl.groups = groups;
-      ctrl.selectedGroup = ctrl.groups[0];
-    });
-
-    ctrl.group = {
-      name: '',
-      items: []
+    ctrl.isEmptyGroup = function () {
+      return (ctrl.selectedDevices.length + ctrl.selectedGroups.length) === 0;
     };
 
     ctrl.saveGroup = function () {
 
+      // Merge devices and groups
       var items = ctrl.selectedDevices.concat(ctrl.selectedGroups);
-      ctrl.group.items = items.map(function(item){
+      ctrl.group.items = items.map(function (item) {
         var a = {};
         a.id = item.id;
         a.type = item.type;
@@ -57,6 +69,10 @@
         ctrl.message = 'Group saved successfully!';
         ctrl.group.name = '';
         ctrl.group.items = [];
+        ctrl.selectedDevice = ctrl.devices[0];
+        ctrl.selectedDevices = [];
+        ctrl.selectedGroup = ctrl.groups[0];
+        ctrl.selectedGroups = [];
       });
     }
   });
