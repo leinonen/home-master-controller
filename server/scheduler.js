a019ef612acf1b6f0544e20b486aa58b66440fb3
Peter Leinonen <peter@leinonen.se>
2015-08-22 00:51:49 +0200

    Randomization offset. Also using event to fire schedule

--- server/scheduler.js
+++ server/scheduler.js
@@ -28,29 +28,51 @@ function scheduler() {
   schedules.forEach(runSchedule);
 }
 
+function timeOffset(time, offsetMinutes) {
+  var d = new Date();
+  d.setHours(time.substring(0, 2));
+  d.setMinutes(time.substring(3, 5));
+  d.setMinutes(d.getMinutes() + offsetMinutes);
+  return d.toString().substring(16, 21);
+}
+
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+function getTimeStamp(date) {
+  return date.toString().substring(16, 24);
+}
+
 function runSchedule(schedule) {
   var now = new Date();
   var dayMap = ['sun', 'mon', 'tue', 'wed', 'thu', 'fri', 'sat'];
   var currentDay = dayMap[now.getDay()];
-  var currentTime = now.toString().substring(16, 24);
-  var scheduleTime = schedule.time + ':00';
+  var currentTime = getTimeStamp(now);
+  var offset = getRandomInt(0, schedule.random || 0);
+  var scheduleTime = timeOffset(schedule.time, offset) + ':00';
   var times = SunCalc.getTimes(new Date(), Config.location.lat, Config.location.lng);
 
   // Handle sunrise or sunset
   if (schedule.sunrise) {
-    scheduleTime = times.sunrise.toString().substring(16, 24);
+    scheduleTime = getTimeStamp(times.sunrise);
   } else if (schedule.sunset) {
-    scheduleTime = times.sunset.toString().substring(16, 24);
+    scheduleTime = getTimeStamp(times.sunset);
   }
 
   if (scheduleTime === currentTime) {
-    triggerSchedule(schedule, scheduleTime, currentDay, currentTime);
+    Bus.emit('ScheduleTrigger', {
+      schedule: schedule,
+      scheduleTime: scheduleTime,
+      currentDay: currentDay,
+      currentTime: currentTime
+    });
   }
 }
 
 function triggerSchedule(schedule, scheduleTime, currentDay, currentTime) {
-  console.log('scheduler - triggerSchedule -> ' + schedule.name);
-  console.log('scheduler - currentTime: ' + currentTime + ', scheduleTime: ' + scheduleTime);
+  console.log('scheduler.triggerSchedule -> (' + schedule.name + ', currentTime: ' + currentTime +
+    ', scheduleTime: ' + scheduleTime + ', random: ' + schedule.random + ')');
 
   if (schedule.sunrise) {
     console.log('scheduler - sunrise trigger - ' + scheduleTime);
@@ -67,8 +89,12 @@ function triggerSchedule(schedule, scheduleTime, currentDay, currentTime) {
   }
 }
 
+function scheduleTrigger(message) {
+  triggerSchedule(message.schedule, message.scheduleTime, message.currentDay, message.currentTime);
+}
+
 function triggerDevice(item, action) {
-  console.log('scheduler - triggerDevice: (' + item.id + ' ' + item.type + ') -> ' + action);
+  console.log('scheduler.triggerDevice -> (' + item.id + ', ' + item.type + ', ' + action + ')');
   Master.control(item.id, {
     action: action,
     type: item.type
@@ -79,6 +105,7 @@ function triggerDevice(item, action) {
 
 function startScheduler() {
   Bus.on('SchedulerUpdate', getSchedules);
+  Bus.on('ScheduleTrigger', scheduleTrigger);
   Bus.emit('SchedulerUpdate');
   schedulerHandle = setInterval(scheduler, 1000);
 }
