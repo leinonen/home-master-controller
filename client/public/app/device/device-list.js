f3ebb63d2e566951509168a0d17e3d8355ab54f3
Leinonen <peter@leinonen.se>
2015-09-20 20:16:08 +0200

    changed device list layout

--- client/public/app/device/device-list.js
+++ client/public/app/device/device-list.js
@@ -5,6 +5,19 @@
   module.controller('DeviceListCtrl', function ($rootScope, $timeout, MasterApi, Message) {
     var ctrl = this;
     ctrl.devices = [];
+    ctrl.controlsActive = {};
+
+    ctrl.showControls = function(device) {
+      return ctrl.controlsActive[device.id] === true;
+    };
+
+    ctrl.toggleShowControls = function(device) {
+      if (ctrl.controlsActive[device.id] === undefined) {
+        ctrl.controlsActive[device.id] = true;
+      } else {
+        ctrl.controlsActive[device.id] = !ctrl.controlsActive[device.id];
+      }
+    };
 
     function fetchDevices() {
       MasterApi.getDevices().then(function (devices) {
@@ -14,30 +27,87 @@
       });
     }
 
-    function fetchDevice(id, type) {
-      MasterApi.getDevice(id, type).then(function (device) {
-        for(var i=0; i<ctrl.devices.length; i++){
-          if (ctrl.devices[i].id === device.id){
-            ctrl.devices[i] = device;
-            console.log('updated device ' + device.id + ' with index ' + i);
-            break;
-          }
-        }
-      }).catch(function (err) {
-        Message.danger(err.data.statusCode + ' : ' + err.data.message);
-      });
-    }
-
     fetchDevices();
 
-    $rootScope.$on('fetchDevices', fetchDevices);
 
-    $rootScope.$on('fetchDevice', function(event, data){
-      fetchDevice(data.id, data.type);
-    });
+    function fetchAndUpdate(device) {
+      $timeout(function () {
+        MasterApi.getDevice(device.id, device.type)
+        .then(function(dev) {
+          for(var i=0; i<ctrl.devices.length; i++){
+            if (ctrl.devices[i].id === dev.id) {
+              ctrl.devices[i] = dev;
+              break;
+            }
+          }
+        })
+        .catch(function (err) {
+          Message.danger(err.data.statusCode + ' : ' + err.data.message);
+        });
+      }, 150);
+    }
+
+
+    function control(device, params) {
+      params.type = device.type;
+      MasterApi.control(device.id, params)
+      .then(function(status) {
+        fetchAndUpdate(device);
+      })
+      .catch(console.error);
+    }
 
     ctrl.showDevices = function () {
       return ctrl.devices.length > 0;
+    };
+
+
+    ctrl.getState = function (device) {
+      if (device.motorized) {
+        return device.state.on ? 'Up' : 'Down';
+      } else {
+        return device.state.on ? 'On' : 'Off';
+      }
+    };
+
+    ctrl.buttonText = function (device) {
+      if (device.motorized) {
+        return device.state.on ? 'Bring Down' : 'Bring Up';
+      } else {
+        return device.state.on ? 'Turn Off' : 'Turn On';
+      }
+    };
+
+    ctrl.colorLoopText = function (device) {
+      return device.state.effect === 'none' ? 'Enable' : 'Disable';
+    };
+
+    ctrl.toggleColorLoop = function (device) {
+      if (device.state.effect === 'none') {
+        control(device, {action: 'colorloop-on'});
+      } else {
+        control(device, {action: 'colorloop-off'});
+      }
+    };
+
+    ctrl.toggleDevice = function (device) {
+      if (device.state.on) {
+        control(device, {action: device.motorized ? 'down' : 'off'});
+      } else {
+        control(device, {action: device.motorized ? 'up' : 'on'});
+      }
+    };
+
+    ctrl.setBrightness = function (device) {
+      control(device, {action: 'bri', value: device.state.bri});
+    };
+
+    ctrl.setSaturation = function (device) {
+      control(device, {action: 'sat', value: device.state.sat});
+    };
+
+    ctrl.setHue = function (device) {
+      control(device, {action: 'hue', value: device.state.hue});
     };
 
   });
