0096e6d38524516756a70c57dcf49329a447559c
Peter Leinonen <peter@leinonen.se>
2015-10-05 19:18:34 +0200

    update sensor list every 10 seconds

--- client/public/app/sensor/sensor-list.js
+++ client/public/app/sensor/sensor-list.js
@@ -2,12 +2,18 @@
 
   var module = angular.module('sensor', ['master']);
 
-  module.controller('SensorListCtrl', function (MasterApi) {
+  module.controller('SensorListCtrl', function (MasterApi, $interval) {
     var ctrl = this;
     this.sensors = [];
     MasterApi.getSensors().then(function (sensors) {
       ctrl.sensors = sensors;
     });
+
+    $interval(function() {
+      MasterApi.getSensors().then(function (sensors) {
+        ctrl.sensors = sensors;
+      });
+    }, 10000);
   });
 
   module.directive('sensorList', function () {
