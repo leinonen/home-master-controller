e731e118a82f446b4e57cf75f1dac031c1d5f82f
Peter Leinonen <peter@leinonen.se>
2015-11-18 21:04:45 +0100

    Cleanup dependencies. Keeping it small

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,17 +1,17 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
-var minifyCSS = require('gulp-minify-css');
+//var minifyCSS = require('gulp-minify-css');
 var path = require('path');
-var Server = require('karma').Server;
-var jasmine = require('gulp-jasmine');
+//var Server = require('karma').Server;
+//var jasmine = require('gulp-jasmine');
 
 gulp.task('less', function () {
   return gulp.src('./client/less/hmc.less')
     .pipe(less())
-    .pipe(minifyCSS())
+//    .pipe(minifyCSS())
     .pipe(gulp.dest('./client/public'));
 });
-
+/*
 gulp.task('test', function (done) {
   new Server({
     configFile: path.join(__dirname, './client/karma.conf.js'),
@@ -23,5 +23,5 @@ gulp.task('jasmine', function () {
   return gulp.src('./server/spec/scheduler.spec.js')
     .pipe(jasmine());
 });
-
+*/
 gulp.task('default', ['less']);
