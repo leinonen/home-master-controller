0880ebbf34b05f818f3813e61a45bd4f91b7f1ec
Peter Leinonen <peter@leinonen.se>
2016-01-31 11:21:16 +0100

    less

--- src/gulpfile.js
+++ src/gulpfile.js
@@ -1,14 +1,21 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
+var concat = require('gulp-concat');
+var cssnano = require('gulp-cssnano');
 //var minifyCSS = require('gulp-minify-css');
 var path = require('path');
 //var Server = require('karma').Server;
 //var jasmine = require('gulp-jasmine');
 
 gulp.task('less', function () {
-  return gulp.src('./client/less/hmc.less')
-    .pipe(less())
+  //return gulp.src('./client/public/app/**/*.less')
+  return gulp.src('./client/public/app/**/*.less')
+    .pipe(less({
+      paths: ['./client/less']
+    }))
 //    .pipe(minifyCSS())
+    .pipe(concat('hmc.css'))
+    .pipe(cssnano())
     .pipe(gulp.dest('./client/public'));
 });
 /*
