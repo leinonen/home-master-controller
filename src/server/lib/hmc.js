9db0b6ed5eafc47a5b0a6b5f571a815c0807faa5
Peter Leinonen <peter@leinonen.se>
2016-07-12 22:49:18 +0200

    Refactor

--- src/server/lib/hmc.js
+++ src/server/lib/hmc.js
@@ -2,51 +2,11 @@
 
 const
   Rx = require('rx'),
-  Hue = require('../lib/hue/hue'),
-  ZWave = require('../lib/zwave/zwave'),
-  Telldus = require('../lib/telldus/telldus'),
-  DeviceTypes = require('../lib/device-types'),
   ServiceHandler = require('../lib/service.handler'),
-  winston = require('winston');
+  winston = require('winston'),
+  integrations = require('./integrations');
 
-
-function HomeMasterController(customIntegrations) {
-  let integrations = customIntegrations || [
-    {
-      name:              'ZWave',
-      type:              'device',
-      types: [           DeviceTypes.ZWAVE_SWITCH_BINARY,
-                         DeviceTypes.ZWAVE_SWITCH_MULTILEVEL
-      ],
-      deviceHandler:     ZWave.device,
-      deviceTransformer: ZWave.transformDevice,
-      listHandler:       ZWave.devices,
-      listTransformer:   ZWave.transformDevices,
-      controlHandler:    ZWave.control
-    },
-    {
-      name:              'Philips Hue',
-      type:              'device',
-      types: [           DeviceTypes.HUE_DEVICE
-      ],
-      deviceHandler:     Hue.light,
-      deviceTransformer: Hue.transformDevice,
-      listHandler:       Hue.lights,
-      listTransformer:   Hue.transformDevices,
-      controlHandler:    Hue.control
-    },
-    {
-      name:              'Telldus Live',
-      type:              'device',
-      types: [           DeviceTypes.TELLDUS_DEVICE
-      ],
-      deviceHandler:     Telldus.device,
-      deviceTransformer: Telldus.transformDevice,
-      listHandler:       Telldus.devices,
-      listTransformer:   Telldus.transformDevices,
-      controlHandler:    Telldus.control
-    }
-  ];
+function HomeMasterController() {
 
   return {
 
@@ -57,13 +17,12 @@ function HomeMasterController(customIntegrations) {
     getDevices: () => {
       return Rx.Observable.from(
         integrations
-        .filter(integration => integration.type === 'device')
-        .map(integration => Rx.Observable.fromPromise(
-          integration
-            .listHandler()
-            .then(integration.listTransformer)
-            .catch(ServiceHandler.noServices)
-        )))
+          .map(integration => Rx.Observable.fromPromise(
+            integration
+              .listHandler()
+              .then(integration.listTransformer)
+              .catch(ServiceHandler.noServices)
+            )))
         .flatMap(x => x)
         .tap(devices => winston.info('HMC.getDevices', devices.length))
         .toPromise();
@@ -78,14 +37,13 @@ function HomeMasterController(customIntegrations) {
     getDevice: (id, type) => {
       return Rx.Observable.from(
         integrations
-          .filter(integration => integration.type === 'device')
           .filter(integration => integration.types.some(t => t === type))
           .map(integration => Rx.Observable.fromPromise(
-          integration
-            .deviceHandler(id)
-            .then(integration.deviceTransformer)
-            .catch(ServiceHandler.noService)
-        ))
+            integration
+              .deviceHandler(id)
+              .then(integration.deviceTransformer)
+              .catch(ServiceHandler.noService)
+          ))
         )
         .flatMap(x => x)
         .tap(device => winston.info('HMC.getDevice', device.id, device.type))
@@ -101,7 +59,6 @@ function HomeMasterController(customIntegrations) {
     control: (id, params) => {
       return Rx.Observable.from(
         integrations
-          .filter(integration => integration.type === 'device')
           .filter(integration => integration.types.some(t => t === params.type))
           .map(integration => Rx.Observable.fromPromise(
             integration
