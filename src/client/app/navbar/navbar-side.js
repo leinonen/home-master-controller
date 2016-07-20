8c035df541fce9d96ec5a0d7baf24c9c104a03d4
Peter Leinonen <peter@leinonen.se>
2016-07-20 21:22:48 +0200

    Split navbar stuff into separate files

--- src/client/app/navbar/navbar-side.js
+++ src/client/app/navbar/navbar-side.js
@@ -3,20 +3,16 @@
 (function() {
 
   angular.module('app')
-    .component('navbar', {
+    .component('navbarSide', {
       bindings: {},
-      templateUrl: 'app/navbar/navbar.html',
-      controller: function(Routes, Auth, $state) {
+      templateUrl: 'app/navbar/navbar-side.html',
+      controller: function(Routes, Auth) {
 
         var ctrl = this;
-        ctrl.title = 'HMC';
 
         ctrl.items = Routes.routes;
+
         ctrl.isLoggedIn = Auth.isLoggedIn;
-        ctrl.logout = function() {
-          Auth.logout();
-          $state.transitionTo('root.devices');
-        };
 
         function hideLogin(item) {
           var loggedIn = Auth.isLoggedIn();
@@ -26,12 +22,6 @@
         ctrl.getLeftItems = function() {
           return ctrl.items.filter(function(item) {
             return item.position === 'left' && hideLogin(item);
-          });
-        };
-
-        ctrl.getRightItems = function() {
-          return ctrl.items.filter(function(item) {
-            return item.position === 'right' && hideLogin(item);
           });
         };
 
